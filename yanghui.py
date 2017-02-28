b4e52c01e8fe0d5e16cf9b343ddd14b200f9ff8f
fiso <fiso0@126.com>
2017-02-28 16:01:45 +0800

    使用函数和生成器生成杨辉三角

---
+++ yanghui.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+def triangles_func(n):  # n = 0, print [1], n = 1, print [1, 1]
+	if n == 0:
+		numbers = [1]
+	elif n == 1:
+		numbers = [1,1]
+	else:
+		old = triangles_func(n - 1)
+		numbers = []
+		numbers.append(1)
+		for t in range(1,n):
+			num = old[t-1] + old[t]
+			numbers.append(num)
+		numbers.append(1)
+	return numbers
+
+
+print('test triangles_func:')
+for m in range(10):
+	print(triangles_func(m))
+
+
+def triangles_func_1(n):  # n = 0, print [1], n = 1, print [1, 1]
+	numbers = []
+	numbers.append(1)  # first 1
+	if n > 1:  # middle numbers
+		old = triangles_func(n - 1)
+		for t in range(1,n):
+			num = old[t-1] + old[t]
+			numbers.append(num)
+	if n > 0:  # last 1
+		numbers.append(1)
+	return numbers
+
+
+print('test triangles_func_1:')
+for m in range(10):
+	print(triangles_func_1(m))
+
+
+def triangles_func_1_ever():  # NO parameter
+	n = 0
+	while True:
+		numbers = []
+		numbers.append(1)  # first 1
+		if n > 1:  # middle numbers
+			old = triangles_func(n - 1)
+			for t in range(1,n):
+				num = old[t-1] + old[t]
+				numbers.append(num)
+		if n > 0:  # last 1
+			numbers.append(1)
+		n += 1
+		return numbers
+
+def triangles():
+	# n = 1
+	# list = []
+	# new_list = [1]
+	# while True:
+	# 	for i in range(n - 1):
+	# 		new_list[n-1] = list[0 if n <= 1 else n - 2] + list[n-1]
+	# 	if n > 1:
+	# 		new_list[n - 1] = 1
+	# 	list = new_list
+	# 	n = n + 1
+	# 	yield new_list
+	n = 0
+	while True:
+		numbers = []
+		numbers.append(1)  # first 1
+		if n > 1:  # middle numbers
+			old = triangles_func(n - 1)
+			for t in range(1,n):
+				num = old[t-1] + old[t]
+				numbers.append(num)
+		if n > 0:  # last 1
+			numbers.append(1)
+		yield numbers
+		n += 1
+
+
+print('test triangles:')
+n = 0
+for t in triangles():
+	print(t)
+	n += 1
+	if n == 10:
+		break
+
+
+def triangles_answers():  # 答案
+	L = [1]
+	while True:
+		yield L
+		L.append(0)
+		L = [L[i-1] + L[i] for i in range(len(L))]
