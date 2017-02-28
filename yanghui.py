527e16e8b11f0a9dc48ed0ec06942eb524e58fb5
fiso <fiso0@126.com>
2017-02-28 17:00:43 +0800

    更新

--- yanghui.py
+++ yanghui.py
@@ -27,7 +27,7 @@ def triangles_func_1(n):  # n = 0, print [1], n = 1, print [1, 1]
 	numbers = []
 	numbers.append(1)  # first 1
 	if n > 1:  # middle numbers
-		old = triangles_func(n - 1)
+		old = triangles_func_1(n - 1)
 		for t in range(1,n):
 			num = old[t-1] + old[t]
 			numbers.append(num)
@@ -41,31 +41,12 @@ for m in range(10):
 	print(triangles_func_1(m))
 
 
-def triangles_func_1_ever():  # NO parameter
-	n = 0
-	while True:
-		numbers = []
-		numbers.append(1)  # first 1
-		if n > 1:  # middle numbers
-			old = triangles_func(n - 1)
-			for t in range(1,n):
-				num = old[t-1] + old[t]
-				numbers.append(num)
-		if n > 0:  # last 1
-			numbers.append(1)
-		n += 1
-		return numbers
-
 def triangles():
 	n = 0
+	numbers = [1]
 	while True:
-		numbers = []
-		numbers.append(1)  # first 1
 		if n > 1:  # middle numbers
-			old = triangles_func(n - 1)
-			for t in range(1,n):
-				num = old[t-1] + old[t]
-				numbers.append(num)
+			numbers = [1]+[numbers[t-1] + numbers[t] for t in range(1,n)]
 		if n > 0:  # last 1
 			numbers.append(1)
 		yield numbers
@@ -81,9 +62,9 @@ for t in triangles():
 		break
 
 
-def triangles_answers():  # 答案
+def triangles_answers():  # 最佳答案
 	L = [1]
 	while True:
 		yield L
 		L.append(0)  # 最后补0
-		L = [L[i-1] + L[i] for i in range(len(L))]  # 前两项相加
+		L = [L[i-1] + L[i] for i in range(len(L))]  # 前两项相加，Python中下标可以是-1
