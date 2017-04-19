71485614d253f788e9d0b6718ce9ea9407b73173
fiso <fiso0@126.com>
2017-04-19 14:23:41 +0800

    增加逆转义函数detran7e

--- JTT808/tran7e.py
+++ JTT808/tran7e.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 def tran7e(msg):
+	# 转义
+	# 消息不带空格
 	msg_len = len(msg)
 	new_msg = msg
 	i = 0
@@ -15,7 +17,24 @@ def tran7e(msg):
 		print('tran7e result: '+new_msg)
 	return new_msg
 
+def detran7e(msg):
+	# 逆转义
+	# 消息不带空格
+	msg_len = len(msg)
+	new_msg = msg
+	i = 0
+	while(i<msg_len):
+		if new_msg[i:i+4] == '7d02':
+			new_msg = new_msg[:i] + '7e' + new_msg[i+4:]
+		elif new_msg[i:i+4] == '7d01':
+			new_msg = new_msg[:i] + '7d' + new_msg[i+4:]
+		i += 2
+	if new_msg != msg:
+		print('tran7e result: '+new_msg)
+	return new_msg
+
 if __name__ == '__main__':
 	test = input('input:')
-	res = tran7e(test)
+	# res = tran7e(test)
+	res = detran7e(test)
 	print(res)
