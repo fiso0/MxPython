f8f18a9bc4e4f17c12d4aa69edcf0d01d4c84af6
fiso <fiso0@126.com>
2017-10-13 11:20:44 +0800

    发送方糖消息

--- taobaoPriceBug/taobaoPriceBug2.py
+++ taobaoPriceBug/taobaoPriceBug2.py
@@ -3,8 +3,18 @@
 
 from selenium import webdriver
 import time
+import requests
 
-#使用selenium
+def send_notice(product, prices):
+	text = (product if (product is not None) else '') + '降价啦'
+	desp = prices[0] + '降到' + prices[1]
+	content = "https://sc.ftqq.com/SCU13848Tc8c8c82df0c2f7a251d8676d2d87245a59e029d80bf3e.send?text=%s&desp=%s" % (
+	text, desp)
+	requests.get(content)
+	print('已下发方糖消息')
+
+
+# 使用selenium
 driver = webdriver.PhantomJS(executable_path="D:\\tools\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe")
 driver.maximize_window()
 
@@ -16,8 +26,9 @@ time.sleep(1)
 try:
 	prices = [e.text for e in driver.find_elements_by_class_name('tm-price')]
 	try:
-		print('商品原价：'+prices[0]+'\n商品优惠价：'+prices[1])
+		print('商品原价：' + prices[0] + '\n商品优惠价：' + prices[1])
+		send_notice(None, prices)
 	except:
-		print('查询结果：'+str(prices))
+		print('查询结果：' + str(prices))
 except:
 	print('查询价格失败')
