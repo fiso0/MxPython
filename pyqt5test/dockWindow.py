aec89e73056451fb1c164139687aa1498c95126a
fiso <fiso0@126.com>
2019-03-28 17:14:10 +0800

    优化

--- pyqt5test/dockWindow.py
+++ pyqt5test/dockWindow.py
@@ -20,23 +20,25 @@ class DockDemo(QMainWindow):
         file.addAction("quit")
 
         # 创建可停靠的窗口
-        self.items = QDockWidget("Dockable", self)
+        self.dock = QDockWidget("Dockable", self)
+        # self.dock.setFeatures(QDockWidget.DockWidgetFloatable)  # 改为只允许float，不允许关闭、移动
+        # 特性可选DockWidgetClosable|DockWidgetMovable|DockWidgetFloatable
 
         # 在停靠窗口内添加QListWidget对象
         self.listWidget = QListWidget()
         self.listWidget.addItem("item1")
         self.listWidget.addItem("item2")
         self.listWidget.addItem("item3")
-        self.items.setWidget(self.listWidget)
+        self.dock.setWidget(self.listWidget)
 
         # 是否将可停靠窗口置于浮动状态，默认是False，下面这句删掉不影响显示结果
-        self.items.setFloating(False)
+        self.dock.setFloating(False)
 
         # 中央控件
         self.setCentralWidget(QTextEdit())
 
         # 将停靠窗口放在中央控件的右侧
-        self.addDockWidget(Qt.RightDockWidgetArea, self.items)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.dock)
 
         self.setLayout(layout)
         self.setWindowTitle("Dock 例子")
