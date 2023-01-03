fa5d652c2fabbd8ce3bd1e0521f9b5ccc663d69c
fiso0 <fiso0@126.com>
2023-01-03 09:59:40 +0800

    update

---
+++ binMerge/binMerge_v1.0.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3.5
+# -*- coding: utf-8 -*-
+
+'''
+合并多个bin文件到一个bin文件
+合并结果文件：MERGE_FILE_NAME
+	默认为merge.bin
+待合并文件个数：BIN_FILE_NUMBER
+	默认为3
+待合并文件名：BIN_FILE_NAMES
+	stm32h743vi-bootloader.bin
+	gap.bin
+	mxt2007.bin
+待合并文件地址：ADDRESS
+	0x08000000
+	0x08020000
+	0x08021000
+'''
+
+MERGE_FILE_NAME = "merge.bin"
+
+BIN_FILE_NUMBER = 3 # bin文件个数为3
+BIN_FILE_NAMES = []
+BIN_FILE_NAMES.append("stm32h743vi-bootloader.bin") # bin文件名1
+BIN_FILE_NAMES.append("gap.bin") # bin文件名2
+BIN_FILE_NAMES.append("mxt2007.bin") # bin文件名3
+
+ADDRESS = []
+ADDRESS.append(0x08000000) # bin文件1地址
+ADDRESS.append(0x08020000) # bin文件2地址
+ADDRESS.append(0x08021000) # bin文件3地址
+
+###############################################################
+import struct
+
+# open files
+bin_files = []
+bin_sizes = []
+for file_name in BIN_FILE_NAMES:
+	try:
+		with open(file_name,'rb') as file:
+			data = file.read()
+			bin_files.append(data)
+			bin_sizes.append(len(data))
+			file.close()
+			print("file read: " + file_name)
+	except FileNotFoundError:
+		print("No file: " + file_name)
+	except Exception as e:
+		print(e)
+
+# calc gaps
+gaps = []
+gaps.append(0)
+i = 1
+while i < BIN_FILE_NUMBER:
+	gap = ADDRESS[i] - ADDRESS[i-1] - bin_sizes[i-1]
+	if gap < 0:
+		print("file %s too big!!!" % BIN_FILE_NAMES[i-1])
+		exit(0)
+	else:
+		gaps.append(gap)
+		i += 1
+
+# create/overwrite file
+try:
+	f_merge = open(MERGE_FILE_NAME, 'xb+')
+	print("merge file create done...")
+except FileExistsError:
+	f_merge = open(MERGE_FILE_NAME, 'wb+')
+	print("merge file already exist, overwrite...")
+except Exception as e:
+	print(e)
+
+# write file data
+for file_name, file_data, gap in zip(BIN_FILE_NAMES,bin_files,gaps):
+	# f_merge.seek(offset)
+	for i in range(gap):
+		f_merge.write(struct.pack('B', 0xff))
+	f_merge.write(file_data)
+	print("file data write: " + file_name)
+
+# close file
+f_merge.close()
+print("merge file close done...")
